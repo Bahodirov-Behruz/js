facaf7a5d8a51cfd826b1f29ab3c34ae7b3cdcfd
Behruz <bahodirovbehruz111@gmail.com>
2024-03-18 13:23:18 +0500

    new change

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-var num1 = prompt("Iltimos birinchi sonni kiriting?");
-var opr = prompt("Iltimos ishorarani kiriting");
-var num2 = prompt("Iltimos ikkinchi sonni kiriting?");
-if (opr == "+") {
-  console.log( +num1 + +num2);
-} else if (opr == "-") {
-  console.log(num1 - num2);
-} else if (opr == "*") {
-  console.log(num1 * num2);
-} else if (opr == "/") {
-  console.log(num1 / num2);
-} else {
-  console.log("error");
-}
+// var num1 = prompt("Iltimos birinchi sonni kiriting?");
+// var opr = prompt("Iltimos ishorarani kiriting");
+// var num2 = prompt("Iltimos ikkinchi sonni kiriting?");
+// if (opr == "+") {
+//   console.log( +num1 + +num2);
+// } else if (opr == "-") {
+//   console.log(num1 - num2);
+// } else if (opr == "*") {
+//   console.log(num1 * num2);
+// } else if (opr == "/") {
+//   console.log(num1 / num2);
+// } else {
+//   console.log("error");
+// }
 
 // let kalkulator = minus;
 // if (kalkulator == plus) {
@@ -38,3 +38,74 @@ if (opr == "+") {
 // }
 
 // console.log(folse || " havo bulutli")
+
+// var opr = prompt("balni kiriting");
+
+// if (opr == " 90 > 100") {
+//   console.log("Siz uqishga grant asosida qabul qilindingiz");
+// } else if ("90 < 75") {
+//   console.log("Siz uqishga kantrak asosida uqisiz");
+// } else if ("75 < 1") {
+//   console.log("Siz uqishga supper kantrak asosida uqisiz");
+// } else "saytda xatolik bor";
+
+// var num1 = prompt("Iltimos birinchi sonni kiriting?");
+// var opr = prompt("Iltimos ishorarani kiriting");
+// var num2 = prompt("Iltimos ikkinchi sonni kiriting?");
+
+// switch (opr == "+") {
+//   case "+":
+//     console.log(+num1 + +num2);
+//     break;
+//     case "-"
+//     // console.log( num1 - num2);
+// } 
+let oy = prompt("Qidiryotgan oyning raqamini kiriting");
+switch (month) {
+  case "1":
+    console.log("Yanvar");
+}
+switch (oy) {
+  case "2":
+    console.log("February");
+}
+switch (oy) {
+  case "3":
+    console.log("March ");
+}
+switch (oy) {
+  case "4":
+    console.log("April");
+}
+switch (oy) {
+  case "5":
+    console.log("May");
+}
+switch (oy) {
+  case "6":
+    console.log("Jun");
+}
+switch (oy) {
+  case "7":
+    console.log("July");
+}
+switch (oy) {
+  case "8":
+    console.log("August");
+}
+switch (oy) {
+  case "9":
+    console.log("Sentober");
+}
+switch (oy) {
+  case "10":
+    console.log("Oktober");
+}
+switch (oy) {
+  case "11":
+    console.log("November");
+}
+switch (oy) {
+  case "12":
+    console.log("December");
+}
